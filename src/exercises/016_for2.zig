3d43d4389e17404255080895b58d7ad55ed851aa
Brad Swagart <bswagart@betterworldbooks.com>
2025-01-30 10:20:46 -0500

    More exercises

--- src/exercises/016_for2.zig
+++ src/exercises/016_for2.zig
@@ -18,14 +18,14 @@ const std = @import("std");
 pub fn main() void {
     // Let's store the bits of binary number 1101 in
     // 'little-endian' order (least significant byte or bit first):
-    const bits = [_]u8{ 1, 0, 1, 1 };
+    const bits = [_]u8{ 1, 0, 1, 1 }; // this would be 1101 in binary cuz it's flipped
     var value: u32 = 0;
 
     // Now we'll convert the binary bits to a number value by adding
     // the value of the place as a power of two for each bit.
     //
     // See if you can figure out the missing pieces:
-    for (bits, ???) |bit, ???| {
+    for (bits, 0..) |bit, i| {
         // Note that we convert the usize i to a u32 with
         // @intCast(), a builtin function just like @import().
         // We'll learn about these properly in a later exercise.
